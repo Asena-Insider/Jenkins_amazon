ffb15a08f61a2b56b54f4b2179112c47b3188255
asena.karakas <asena.karakas@useinsider.com>
2025-06-11 23:33:47 +0300

    Adam ol güncelle Samet

--- tests/test_amazon.py
+++ tests/test_amazon.py
@@ -12,7 +12,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+
 
 class AmazonTest(unittest.TestCase):
 
